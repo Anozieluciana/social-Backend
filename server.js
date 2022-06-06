156ff59bfaf3ac122300e20702b06abfc520fbad
Anozieluciana <anoziechiderasilverlin@gmail.com>
2022-06-06 18:36:49 +0100

    changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors')
 require("dotenv").config()
 
-const port = process.env.Port || 3322
+const port = process.env.PORT || 3322
 const app = express();
 
 app.use(cors());
