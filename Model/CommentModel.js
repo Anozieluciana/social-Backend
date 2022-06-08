ef855780d92cbb3e04f79af3ff5d629f39297412
Anozieluciana <anoziechiderasilverlin@gmail.com>
2022-06-08 11:20:58 +0100

    controller

--- Model/CommentModel.js
+++ Model/CommentModel.js
@@ -1,41 +1,18 @@
-const mongoose = require('mongoose')
-const userSchema = mongoose.Schema({
-    
-        fullname:{
-            type:String,
-        },
-        username:{
-            type:String,
-        },
-        email:{
-            type:String,
-            unique:true
-        },
-        password:[
-            {
-            type:String
-        },
-    ],
-        avatar:
-        {
-            type:String
-        },
-        avatarID:
-        {
-            type:String
-        },
-        followings:[
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"users"
-        },
-        ],
-        followers:[
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"users"
-        },
-        ],
-    
-}, {Timestamp:true})
-module.exports=mongoose.model("post", userSchema)
+const mongoose = require("mongoose");
+
+const commentModel = mongoose.Schema(
+	{
+		post: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "posts",
+		},
+
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "users",
+		},
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model("comments", commentModel);
